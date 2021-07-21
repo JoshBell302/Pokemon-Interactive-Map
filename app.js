eb1eccd14ffe688f7f766fc92466c1d19bba81cd
Josh Bell <bellj3@oregonstate.edu>
2021-07-21 02:30:52 -0700

    Made a header and footer
    Added images and updated the design of the website with a set dark mode

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.set('view engine', 'handlebars');
 app.engine('handlebars', handlebars({layoutsDir: __dirname + '/views/layouts',}));
 app.use(express.static(__dirname));
 
+// ========================= LOADING ==========================
 app.get('/', (req, res) => {
   console.log("The website has requested " + req.url);
   res.render('homepage', {});
 });
 
+// ========================== LISTEN ==========================
 app.listen(port, () => console.log(`Website is listening to port ${port}`));
